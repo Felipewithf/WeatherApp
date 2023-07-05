9d6e96e250b03da770eaeec0d8d0ea66dc00aa81
Felipe <felipe.sarmiento.as@gmail.com>
2023-07-05 16:14:10 -0400

    added dayjs to change the date labeling

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,16 +20,44 @@ function generateHTML(data){
 
 function renderCurrentMainCity(data){
     $('#mainCity').text(data.city.name);
-    $('#mainTemp').text(data.list[0].main.temp);
+    //transform temp to only two decimal places
+    $('#mainTemp').text(`${data.list[0].main.temp}°C`);
+    //transform to remove HH MM SS
+    $('#currentDate').text(dateWithNoTime(data.list[0].dt_txt));
     $('mainWind').text(data.list[0].wind.speed);
     $('mainHumidity').text(data.list[0].main.humidity);
 }
 
 
 function renderFiveDayForecast(data){
-    var fiveDatsForecastEL = $("#fiveDayForecast").children();
-    for (const iterator of fiveDatsForecastEL) {
-        console.log(iterator);
-        
+
+    for (let index = 0; index < data.list.length; index = index + 8) {
+
+            var singleLiEl = $("<li>");
+            $("#fiveDayForecast").append(singleLiEl);
+            //transform from YYYY MM DD hh:mm:ss -> DDD using DAYJS
+            singleLiEl.append($("<h5>").text(dateToShortDay(data.list[index].dt_txt)));
+            //tranform to temperature with no decimal places
+            singleLiEl.append($("<smallTemp>").text(`${data.list[index].main.temp}°C`));
+
+            singleLiEl.append($("<div>",{"class":"smallLabels"})
+                            .append(
+                                $("<p>").text("Wind:"),
+                                $("<p>").text("Humidity:")
+                                ));
+            singleLiEl.append($("<div>",{"class":"smallInfo"})
+                            .append(
+                                $("<p>").text(`${data.list[index].main.humidity} mph`),
+                                $("<p>").text(`${data.list[index].main.humidity} %`)
+                                ));                               
     }
+ 
+}
+
+function dateToShortDay(date){
+    return dayjs(date).format('ddd');
+}
+
+function dateWithNoTime(date){
+    return dayjs(date).format('MMMM D, YYYY')
 }
